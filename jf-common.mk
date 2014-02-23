40712d0091b3528ebf4b71d678fa566ec2794426
sakuramilk <c.sakuramilk@gmail.com>
2014-02-24 00:20:50 +0900

    workaround: change vendor priproetary path jf-common to jflte
    Change-Id: I77104c23f618c564d9765ccd9d068bbc2aa92b31

--- jf-common.mk
+++ jf-common.mk
@@ -17,7 +17,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 ## (2) Also get non-open-source specific aspects if available
-$(call inherit-product-if-exists, vendor/samsung/jf-common/jf-common-vendor.mk)
+$(call inherit-product-if-exists, vendor/samsung/jflte/jflte-vendor.mk)
 
 ## overlays
 DEVICE_PACKAGE_OVERLAYS += device/samsung/jf-common/overlay
